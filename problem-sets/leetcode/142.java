c51a1dd00783f2cc9b89ebff10a55d30e06aecc4
Harsh Gill <harsh.gill@yahoo.com>
2024-06-01 00:42:44 -0600

    Added 142

---
+++ problem-sets/leetcode/142.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+class Solution {
+    public ListNode detectCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return null;
+        }
+        ListNode p1 = head, p2 = head;
+
+        while (p2 != null && p2.next != null) {
+            p1 = p1.next;
+            p2 = p2.next.next;
+            if (p1 == p2) {
+                break;
+            }
+        }
+        if (p2 == null || p2.next == null) {
+                return null;
+        }
+        
+        p1 = head;
+        while (p1 != p2) {
+            p1 = p1.next;
+            p2 = p2.next;
+        }
+        return p1;
+    }
+}
