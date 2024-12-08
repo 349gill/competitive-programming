98e81cb3d560b31a0bf538d666eef925b6fcdd65
Harsh Gill <harsh.gill@yahoo.com>
2024-12-07 19:54:43 -0700

    Renaming

--- problem-sets/leetcode/268.java
+++ problem-sets/leetcode/268.java
@@ -21,3 +21,18 @@ class Solution {
         return i;
     }
 }
+
+
+// Bitwise solution
+
+/**
+ * class Solution {
+    public static int missingNumber(int[] nums) {
+        for (int i = 1; i < nums.length; i++) {
+            nums[0] ^= nums[i];
+            nums[0] ^= i;
+        }
+        return nums[0] ^ nums.length;
+    }
+}
+ */
--- problem-sets/leetcode/268_Bitwise.java
+++
@@ -1,9 +0,0 @@
-class Solution {
-    public static int missingNumber(int[] nums) {
-        for (int i = 1; i < nums.length; i++) {
-            nums[0] ^= nums[i];
-            nums[0] ^= i;
-        }
-        return nums[0] ^ nums.length;
-    }
-}
